56e802db51a4a6fb92dd02faff3bca43f1b6cef5
montoyajosh <mon21012@byui.edu>
2021-10-15 12:50:18 +0800

    commit

---
+++ lesson5/js/date.js
@@ -0,0 +1,29 @@
+document.getElementById("copyrightyear").textContent = new Date().getFullYear();
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric"
+}
+document.getElementById("currentdate").textContent = new Date().toLocaleDateString('PST', options)
+const menubutton = document.querySelector('.menu');
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+menubutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
+hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
+
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("list");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
