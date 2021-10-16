d86587ddef7acdd65b9eb4fb63416fa5fbc3f0df
montoyajosh <mon21012@byui.edu>
2021-10-17 05:12:41 +0800

    commit

--- lesson5/js/date.js
+++ lesson5/js/date.js
@@ -3,37 +3,30 @@ const options1 = {
     weekday: "long",
     day: "numeric",
     month: "long",
-    year: "numeric",
-    hour: "numeric",
-    minute: "numeric"
+    year: "numeric"
 }
+const options2 = {
+    weekday: "long"
+}
+
 document.getElementById("currentdate").textContent = new Date().toLocaleDateString('PST', options1)
 const menubutton = document.querySelector('.menu');
-const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
-
 menubutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
-hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
-
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
 
-var header = document.getElementById("myDIV");
+var header = document.querySelector(".navigation");
 var btns = header.getElementsByClassName("list");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
         var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
+        current[0].className = current[0].className.replace("active", "");
         this.className += " active";
     });
 }
 
-const options2 = {
-    weekday: 'long'
-}
-const day_name = currentdate.toLocaleDateString('PST', options2)
-    // console.log(dayName)
-
+const day_name = new Date().toLocaleDateString('PST', options2)
 if (day_name == "Friday") {
-    document.getElementsByClassName("pancake")[0].classList.toggle("is_tomorrow")
+    document.getElementsByClassName("pancake")[0].classList.toggle("party");
 }
