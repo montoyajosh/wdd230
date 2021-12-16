54c5d7ab46b2e4fdac6d921aba96a065320d0f59
montoyajosh <mon21012@byui.edu>
2021-12-16 20:42:11 +0800

    commit

---
+++ Chamber-of-Commerce/js/events.js
@@ -0,0 +1,19 @@
+function upcomingEvents () {
+    const url = "https://montoyajoshua.github.io/wdd230/Chamber-of-Commerce/json/events.json";
+
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        const events = json['events'];
+            
+        events.forEach(event => {
+            let contents = document.createElement("p");
+            contents.innerHTML = event;
+            document.getElementById("events").appendChild(contents);
+        });
+    }
+  );
+}
+
